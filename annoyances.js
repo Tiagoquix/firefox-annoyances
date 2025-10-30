69ddfd2baaf46a9ec3974d55d9e63e4bd792f0d6
Tiagoquix <30274161+Tiagoquix@users.noreply.github.com>
2025-10-30 00:06:29 -0300

    Annoyances 2.0

--- annoyances.js
+++ annoyances.js
@@ -1,71 +1,50 @@
 // ==================================================================================================================
 // Disable Machine Learning / Artificial Intelligence features
-// https://firefox-source-docs.mozilla.org/toolkit/components/ml/extensions.html
 // ==================================================================================================================
 
-// Core (master switches)
-user_pref("browser.ml.enable", false);
-user_pref("extensions.ml.enabled", false);
+// ML components. See tor-browser#44045.
 
-// AI sidebar chat (also disables AI-related options in other places)
+// tor-browser#42872, #42555, #44045: Disable ML translations.
+user_pref("browser.translations.enable", false);
+
+// Disables many (but not all) ML engines. Note, this does not have overall
+// control over exposure to ML features. tor-browser#44045.
+user_pref("browser.ml.enable", false);
+
+// Disable third party AI chatbot. tor-browser#43989.
 user_pref("browser.ml.chat.enabled", false);
 
-// "Ask an AI Chatbot" context menu item
-// It should be disabled by the above preference, but there seems to be a regression
-// introduced in Firefox 140 where it does not to that, so we have to use an additional
-// preference to get rid of it.
-// https://bugzilla.mozilla.org/show_bug.cgi?id=1994785
-user_pref("browser.ml.chat.page", false);
+// Disable LinkPreview. tor-browser#44045 and tor-browser#43867.
+user_pref("browser.ml.linkPreview.enabled", false);
 
-// Tab grouping AI suggestions
+// Disable Smart Tab Groups. tor-browser#44045.
 user_pref("browser.tabs.groups.smart.enabled", false);
 user_pref("browser.tabs.groups.smart.userEnabled", false);
 
-// ==================================================================================================================
-// Disable tab hover previews
-// Visuals are annoying. Newer previews sometimes don't show the entire tab title.
-// Older preview which only has text loads faster and is nicer on the eyes.
-// ==================================================================================================================
-user_pref("browser.tabs.hoverPreview.enabled", false);
-user_pref("browser.tabs.hoverPreview.showThumbnails", false);
+// Don't expose ModelHub API for extensions. tor-browser#44045.
+user_pref("extensions.ml.enabled", false);
 
-// ==================================================================================================================
-// Disable "Copy Link without Site Tracking" context menu option
-// Creates confusion/duplication with the normal "Copy Link", and most of the time doesn't make the URL actually clear.
-// It's better to do it yourself or just use ClearURLs for uBlock Origin from Adlift and visit the link to copy.
-// ==================================================================================================================
-user_pref("privacy.query_stripping.strip_on_share.enabled", false);
+// Don't enable ML generated alt text. tor-browser#44045.
+// pdfjs.enableAltText controls whether MLManager is created,
+// pdfjs.enableGuessAltText controls whether the MLManager can create an ML
+// engine. pdfs.enableAltText controls whether the latter preference can be
+// changed by the user in the UI, but also has the side effect of hiding the
+// UI controls for the non-ML preference pdfjs.enableNewAltTextWhenAddingImage.
+// See bugzilla bug 1943456 comment 12.
+user_pref("pdfjs.enableAltText", false);
+user_pref("pdfjs.enableAltTextForEnglish", false);
+user_pref("pdfjs.enableGuessAltText", false);
+user_pref("pdfjs.enableAltTextModelDownload", false);
 
-// ==================================================================================================================
-// Disable full-screen transition and warning
-// Why fade to black when entering full-screen?
-// Even without animations, it's should obvious for most people that you've asked for full-screen.
-// ==================================================================================================================
-user_pref("full-screen-api.transition-duration.enter", "0 0");
-user_pref("full-screen-api.transition-duration.leave", "0 0");
-user_pref("full-screen-api.transition.timeout", 0);
-user_pref("full-screen-api.warning.delay", 0);
-user_pref("full-screen-api.warning.timeout", 0);
+// Disable SuggestBackendMl. tor-browser#44045.
+user_pref("browser.urlbar.quicksuggest.mlEnabled", false);
 
-// ==================================================================================================================
-// Disable pointer lock warning
-// I use the website https://loadout.tf/ for creating cool cosmetic loadouts for my mercenaries in the game
-// Team Fortress 2. But everytime I rotate my character the warning appears, which makes it really obnoxious.
-// ==================================================================================================================
-user_pref("pointer-lock-api.warning.timeout", 0);
-
-// ==================================================================================================================
-// Disable picture-in-picture media controls
-// Multitasking is an illusion. You better either focus on the video or on the contet you're consuming.
-// Picture-in-picture is bad. Watch the video in a bigger proportion.
-// ==================================================================================================================
-user_pref("extensions.pictureinpicture.enable_picture_in_picture_overrides", true);
-user_pref("media.videocontrols.picture-in-picture.enabled", false);
-user_pref("media.videocontrols.picture-in-picture.video-toggle.enabled", false);
+// Disable SemanticHistory search. tor-browser#44045.
+user_pref("places.semanticHistory.featureGate", false);
 
 // ==================================================================================================================
 // Disable various promotions to Mozilla's own services such as:
-// Firefox Mobile app; Mozilla VPN; Mozilla Focus etc.
+// Firefox Mobile app / Mozilla VPN / Mozilla Focus etc.
 // They appear randomly in some sections of the browser
 // ==================================================================================================================
 user_pref("browser.contentblocking.report.hide_vpn_banner", true);
@@ -93,24 +72,3 @@ user_pref("browser.preferences.moreFromMozilla", false);
 
 // Disable link to get more themes in Themes section
 user_pref("lightweightThemes.getMoreURL", "");
-
-// ==================================================================================================================
-// Force URLs to be visually always completely shown and don't hide anything from them
-// Forces padlock (certificates) and HTTPS text to be shown and prefer normal URLs rather than search terms
-// Arkenfox forces Search Terms OFF and enables network.IDN_show_punycode, so they are not included here.
-// ==================================================================================================================
-user_pref("browser.urlbar.trimHttps", false);
-user_pref("browser.urlbar.trimURLs", false);
-
-// ==================================================================================================================
-// Disable ALT key from opening the menu bar (File, Edit etc. actions)
-// ==================================================================================================================
-user_pref("ui.key.menuAccessKeyFocuses", false);
-
-// ==================================================================================================================
-// Linux-specific tweaks
-// Force scrollbar to always be visible
-// Force Firefox to use native file picker (useful for KDE users)
-// ==================================================================================================================
-user_pref("widget.gtk.overlay-scrollbars.enabled", false);
-user_pref("widget.use-xdg-desktop-portal.file-picker", 1);
